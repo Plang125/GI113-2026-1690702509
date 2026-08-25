a82431a82fba60434bf67f5289997ac4e05169af
Plang125 <rathaphol.chao@bumail.net>
2026-08-25 10:48:50 +0700

    Add leb02

---
+++ Leb02/Leb02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID : 1690702509
+ * Name       : Leb02
+ * Section    : 129C
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Leb02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
